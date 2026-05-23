3610fa27541ff78e4a5c43d6fd644f2b7726de34
Benjamin Bojko <benjaminbojko@users.noreply.github.com>
2026-05-23 11:06:46 -0400

    fix(scripts): resolve dry-run scoping bug
    Explicitly bind  parameter to a script-scoped variable in ziptie-init.ps1 to resolve PowerShell 5.1/Sandbox switch dynamic scoping binding bugs across all lockdown scripts.

--- scripts/utils/ziptie-init.ps1
+++ scripts/utils/ziptie-init.ps1
@@ -1,8 +1,9 @@
 # Common initializer for Ziptie lockdown scripts
 
+$script:ziptieDryRun = $DryRun
 $ziptieRegistryHelper = "$PSScriptRoot/ziptie-set-registry.ps1"
 
 $registryTweak = {
     Param([String]$Path, [String]$Name, [Object]$Value, [String]$Type, [Switch]$Remove)
-    & $ziptieRegistryHelper -Path $Path -Name $Name -Value $Value -PropertyType $Type -Remove:$Remove -DryRun:$DryRun
+    & $ziptieRegistryHelper -Path $Path -Name $Name -Value $Value -PropertyType $Type -Remove:$Remove -DryRun:$script:ziptieDryRun
 }
